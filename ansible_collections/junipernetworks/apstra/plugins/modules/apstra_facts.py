8b67c72a947f14e3107d7478b194acb017cd348d
ejacques <ejacques@juniper.net>
2024-08-30 14:38:22 -0400

    support for getting blueprints

--- ansible_collections/junipernetworks/apstra/plugins/modules/apstra_facts.py
+++ ansible_collections/junipernetworks/apstra/plugins/modules/apstra_facts.py
@@ -30,18 +30,35 @@ def main():
         version = client.version.get()
         
         # Get /api/blueprints
+        blueprints = client.blueprints.list()
+        blueprints_map = {blueprint['id']: blueprint for blueprint in blueprints}
         
         # Get /api/blueprints/{blueprint_id}/routing-zone-constraints
+        # for id, blueprint in blueprints_map.items():
+        #     routing_zone_constraints = client.blueprints[id].routing_zone_constraints.list()
+        #     for constraint in routing_zone_constraints:
+        #         blueprint['routing_zone_constraints'][constraint[id]] = constraint
         
-        # Get /api/blueprints/{blueprint_id}/config-templates
+        # # Get /api/blueprints/{blueprint_id}/config-templates
+        # for blueprint in blueprints:
+        #     config_templates = client.blueprints[blueprint['id']].config_templates.get()
         
-        # Get /api/blueprints/{{blueprint_id}}/endpoint-policies
+        # # Get /api/blueprints/{{blueprint_id}}/endpoint-policies
+        # for blueprint in blueprints:
+        #     endpoint_policies = client.blueprints[blueprint['id']].endpoint_policies.get()
         
-        # Get /api/blueprints/{{blueprint_id}}/obj-policy-application-points
+        # # Get /api/blueprints/{{blueprint_id}}/obj-policy-application-points
+        # for blueprint in blueprints:
+        #     obj_policy_application_points = client.blueprints[blueprint['id']].obj_policy_application_points.get()
         
         # Example of gathering facts
         facts = {
             'version': version,
+            'blueprints': blueprints_map,
+            # 'routing_zone_constraints': routing_zone_constraints,
+            # 'config_templates': config_templates,
+            # 'endpoint_policies': endpoint_policies,
+            # 'obj_policy_application_points': obj_policy_application_points,
             # Add more facts as needed
         }
         
--- ansible_collections/junipernetworks/apstra/plugins/modules/authenticate.py
+++ ansible_collections/junipernetworks/apstra/plugins/modules/authenticate.py
@@ -2,39 +2,7 @@ from ansible.module_utils.basic import AnsibleModule
 from aos.sdk.client import Client
 import os
 from ansible_collections.junipernetworks.apstra.plugins.module_utils.apstra.client import apstra_client_module_args, apstra_client_auth_info
-
-def connect(module):
-    api_url, verify_certificates, auth_token = apstra_client_auth_info(module.params)
-    username = module.params['username'] or os.getenv('APSTRA_USERNAME')
-    password = module.params['password'] or os.getenv('APSTRA_PASSWORD')
-    logout = not (module.params['logout'] in ['0', 'false', 'False', 'FALSE', 'no', 'No', 'NO'])
-
-    if not api_url or (not auth_token and (not username or not password)):
-        module.fail_json(msg="Missing required parameters: api_url, auth_token or (username and password)")
-
-    # If auth_token is already set, and we're not logging out, return the auth_token.
-    if hasattr(module, 'auth_token') and not logout:
-        module.exit_json(changed=False, response=module.auth_token)
-        return
-
-    client = Client(api_url, verify_certificates)
-
-    # If not given an auth_token, create one.
-    if not auth_token:
-        auth_token = client.login(username, password)
-    else:
-        client.set_auth_token(auth_token)
-
-    if logout:
-        # Add client to the play context (will clean up later)
-        setattr(module, 'apstra_client', client)
-        module.exit_json(changed=True)
-
-    # Saved to avoid logging in needlessly in the same playbook
-    setattr(module, 'auth_token', auth_token)
-
-    # Return the auth token
-    module.exit_json(changed=True, response=auth_token)
+import sys
 
 def main():
     authenticate_args = dict(
@@ -56,11 +24,42 @@ def main():
     )
 
     try:
-        connect(module)
+        api_url, verify_certificates, auth_token = apstra_client_auth_info(module.params)
+        username = module.params['username'] or os.getenv('APSTRA_USERNAME')
+        password = module.params['password'] or os.getenv('APSTRA_PASSWORD')
+        logout = module.params['logout']
+
+        if not api_url or (not auth_token and (not username or not password)):
+            module.fail_json(msg="Missing required parameters: api_url, auth_token or (username and password)")
+
+        # If auth_token is already set, and we're not logging out, return the auth_token.
+        if hasattr(module, 'auth_token') and hasattr(module, 'username') and not logout:
+            module.exit_json(changed=False, auth_token=module.auth_token, username=module.username)
+            return
+
+        client = Client(api_url, verify_certificates)
+
+        # If not given an auth_token, create one.
+        if auth_token:
+            client.set_auth_token(auth_token)
+        else:
+            auth_token, username = client.login(username, password)
+
+        if logout:
+            # Add client to the play context (will clean up later)
+            setattr(module, 'client', client)
+            module.exit_json(changed=True)
+
+        # Saved to avoid logging in needlessly in the same playbook
+        setattr(module, 'auth_token', auth_token)
+        setattr(module, 'username', username)
+
+        # Return the auth token
+        module.exit_json(changed=True, auth_token=auth_token, username=username)
     except Exception as e:
         module.fail_json(msg=str(e), **result)
     finally:
-        client = getattr(module, 'apstra_client')
+        client = getattr(module, 'client')
         if client:
             client.logout()
 
