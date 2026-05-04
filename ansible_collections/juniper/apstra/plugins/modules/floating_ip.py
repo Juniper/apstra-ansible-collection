c4becc7f89ea3e41bdeba1bfa096c9fb81b1c6f2
vgavini <vgavini@juniper.net>
2026-05-04 05:12:32 -0700

    fix(floating_ip): normalize copyright header to match other modules

--- ansible_collections/juniper/apstra/plugins/modules/floating_ip.py
+++ ansible_collections/juniper/apstra/plugins/modules/floating_ip.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2024 Juniper Networks, Inc.
+# Copyright (c) 2024, Juniper Networks
 # Apache License, Version 2.0 (see https://www.apache.org/licenses/LICENSE-2.0)
 
 from __future__ import absolute_import, division, print_function
