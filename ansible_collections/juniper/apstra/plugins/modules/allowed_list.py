2a3b27f27ef42943089adf476ddb9ec74e15ad3a
Shirish Ranoji <sranoji@juniper.net>
2026-07-07 09:54:24 +0530

    Changing the version of module.

--- ansible_collections/juniper/apstra/plugins/modules/allowed_list.py
+++ ansible_collections/juniper/apstra/plugins/modules/allowed_list.py
@@ -14,7 +14,7 @@ module: allowed_list
 
 short_description: Manage platform-level IP/subnet allow list in Apstra
 
-version_added: "1.2.0"
+version_added: "1.1.0"
 
 author:
   - "Shirish Ranoji (@sranoji)"
